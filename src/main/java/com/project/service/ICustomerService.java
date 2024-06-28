83eac0d2d251dbb53d74eb5bd5ab55c6f733c4af
kordeavinash86@gmail.com <kordeavinash86@gmail.com>
2024-06-28 16:51:06 +0530

    first commit

---
+++ src/main/java/com/project/service/ICustomerService.java
@@ -0,0 +1,15 @@
+package com.project.service;
+
+import com.project.entity.Customer;
+
+import java.util.List;
+
+
+public interface ICustomerService {
+
+    public String registerCustomer(Customer customer);
+    public List<Customer> fetchAllCustomer();
+    public Customer fetchCustomerByfirstName(String firstName);
+    public String updateCustomerByDetails(Customer customer);
+    public String deleteCustomerById(Integer id);
+}
